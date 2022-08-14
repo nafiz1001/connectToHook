69484ffb8445cbf3d6ee683d8c28fea69125ac22
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-14 14:35:34 -0400

    parseFile

--- index.ts
+++ index.ts
@@ -1,26 +1,73 @@
 import fs from "fs"
 import * as parser from "@babel/parser";
-import traverse_ from "@babel/traverse";
-import { CallExpression, Identifier } from "@babel/types";
+import traverse_, { NodePath, Scope } from "@babel/traverse";
+import { CallExpression, Identifier, Node, ExportDefaultDeclaration } from "@babel/types";
 const traverse = (traverse_ as any).default as typeof traverse_;
 
-const filePaths = process.argv.slice(2)
-const content = fs.readFileSync(filePaths[0])
-const ast = parser.parse(content.toString(), { sourceType: "module", plugins: ["jsx"] });
 
+const findNode = <T = Node>(ast: Node, target: Node["type"], condition: (path: NodePath<T>) => boolean, scope?: Scope | undefined, state?: T | undefined, parentPath?: NodePath | undefined) => {
+    let path: NodePath<T> | undefined;
 
-let usesConnect = false
-traverse(ast, {
-    ExportDefaultDeclaration(nodePath) {
-        const node = nodePath.node
-        const afterDefault = node.declaration as CallExpression
+    traverse(
+        ast,
+        {
+            [target](_path: NodePath<T>) {
+                if (condition(_path)) {
+                    path = _path
+                    _path.stop()
+                }
+            }
+        },
+        scope,
+        state,
+        parentPath,
+    )
 
-        const connectCall = afterDefault.callee as CallExpression
-        const connectName = (connectCall.callee as Identifier).name
-        const connectArguments = connectCall.arguments as Identifier[]
-
-        const component = afterDefault.arguments[0] as Identifier
-
-        console.log(`export default ${connectName}(${connectArguments.map((arg) => arg.name).join(", ")})(${component.name});`);
+    if (!path) {
+        throw `${target} not found within lines ${ast.start}-${ast.end}`
     }
-});
+
+    return { path, node: path.node, rest: [path.scope, path.state, path.parentPath] }
+}
+
+const findConnect = (ast: Node) => {
+    const {
+        node: exportDefaultDeclaration,
+        rest: exportDefaultDeclarationRest
+    } = findNode<ExportDefaultDeclaration>(ast, "ExportDefaultDeclaration", () => true)
+
+    const connect = findNode<CallExpression>(exportDefaultDeclaration, "CallExpression", (path) => {
+        return (path.node.callee as Identifier)?.name === "connect"
+    }, ...exportDefaultDeclarationRest).node
+
+    const rightBeforeConnect = findNode<CallExpression>(exportDefaultDeclaration, "CallExpression", (path) => {
+        return path.node.callee === connect
+    }, ...exportDefaultDeclarationRest).node
+
+    const defaultComponent = (rightBeforeConnect.arguments[0] as Identifier)?.name
+
+    if (connect.arguments.length > 2) {
+        throw `${connect.arguments.length} > 2`
+    }
+
+    const [mapStateToPropsNode, actionCreatorsNode] = connect.arguments as Identifier[]
+
+    return {
+        rightBeforeConnect,
+        defaultComponent,
+        mapStateToProps: mapStateToPropsNode?.name,
+        actionCreators: actionCreatorsNode?.name,
+    }
+}
+
+const parseFile = (filePath: string) => {
+    const content = fs.readFileSync(filePath)
+    const ast = parser.parse(content.toString(), { sourceType: "module", plugins: ["jsx"] });
+
+    const { rightBeforeConnect, defaultComponent, mapStateToProps, actionCreators } = findConnect(ast)
+
+    console.log(`connect(${mapStateToProps}, ${actionCreators})(${defaultComponent})`);
+}
+
+const filePaths = process.argv.slice(2)
+parseFile(filePaths[0])
