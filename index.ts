ef372a4f5799cbb980bec227f293cfe0b9c5bd50
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-14 12:37:38 -0400

    use babel

--- index.ts
+++ index.ts
@@ -1,23 +1,26 @@
-const acorn = require("acorn")
-const AcornJSX = require("acorn-jsx")
-const AcornClassFields = require("acorn-class-fields")
-const walk = require("acorn-walk")
-const fs = require("fs")
-
-const JSXParser = acorn.Parser.extend(
-    AcornJSX(),
-    AcornClassFields,
-)
+import fs from "fs"
+import * as parser from "@babel/parser";
+import traverse_ from "@babel/traverse";
+import { CallExpression, Identifier } from "@babel/types";
+const traverse = (traverse_ as any).default as typeof traverse_;
 
 const filePaths = process.argv.slice(2)
-
 const content = fs.readFileSync(filePaths[0])
-const node = JSXParser.parse(content.toString(), { ecmaVersion: "latest", sourceType: "module" })
-walk.simple(node, {
-    ExportDefaultDeclaration(node: any) {
-        console.log(node);
+const ast = parser.parse(content.toString(), { sourceType: "module", plugins: ["jsx"] });
+
+
+let usesConnect = false
+traverse(ast, {
+    ExportDefaultDeclaration(nodePath) {
+        const node = nodePath.node
+        const afterDefault = node.declaration as CallExpression
+
+        const connectCall = afterDefault.callee as CallExpression
+        const connectName = (connectCall.callee as Identifier).name
+        const connectArguments = connectCall.arguments as Identifier[]
+
+        const component = afterDefault.arguments[0] as Identifier
+
+        console.log(`export default ${connectName}(${connectArguments.map((arg) => arg.name).join(", ")})(${component.name});`);
     }
-}, {
-    ...walk.base,
-    JSXElement: () => { },
-})
+});
