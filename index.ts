d5b7d5749b74e0cba71057d9fa31bc8c63910050
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-22 14:42:44 -0400

    comment code

--- index.ts
+++ index.ts
@@ -67,6 +67,7 @@ const parseFile = (filePath: string) => {
         return (path.node.declarations[0].id as Identifier)?.name === mapStateToProps
     })
 
+    // key value pairs in mapStateProps
     const propsToState = (((mapStateToPropsDeclaration.node.declarations[0].init as ArrowFunctionExpression).body as ObjectExpression).properties as ObjectProperty[]).map((prop) => {
         const key = (prop.key as Identifier).name
         const valueExpression = (prop.value as MemberExpression)
@@ -79,6 +80,7 @@ const parseFile = (filePath: string) => {
         return (path.node.declarations[0].id as Identifier)?.name === actionCreators
     })
 
+    // all the actions from redux
     const actions = ((actionCreatorsDeclaration.node.declarations[0].init as ObjectExpression).properties as ObjectProperty[]).map((prop) => {
         const key = (prop.key as Identifier).name
         return key
@@ -88,32 +90,43 @@ const parseFile = (filePath: string) => {
         return (path.node.declarations[0].id as Identifier)?.name === defaultComponent
     })
 
+    console.log('import { useDispatch, useSelector } from "react-redux";')
+
     const defaultComponentFunction = defaultComponentDeclaration.node.declarations[0].init as ArrowFunctionExpression
     const defaultComponentParams = defaultComponentFunction.params[0] as ObjectPattern
     const defaultComponentBody = defaultComponentFunction.body as BlockStatement
 
+    // regions of lines to skip including mapStateProps and actionCreators
+    // I don't know which is first, so I sort
     const skipLines = [mapStateToPropsDeclaration.node, actionCreatorsDeclaration.node].map((node) => {
 	return [node.start, node.end] as number[]
     }).sort((a, b) => a[0] - b[0])
 
-    // console.log(content.substring(0, skipLines[0][0]))
-    // console.log(content.substring(skipLines[0][1], skipLines[1][0]))
-    // console.log(content.substring(skipLines[1][1], defaultComponentParams.start as number - 1))
+    // print every from top to the default component except mapStateProps and actionCreators
+    console.log(content.substring(0, skipLines[0][0]))
+    console.log(content.substring(skipLines[0][1], skipLines[1][0]))
+    console.log(content.substring(skipLines[1][1], defaultComponentParams.start as number - 1))
+
+    // print default component signature
     console.log(`const ${defaultComponent} = (${[...actions, ...propsToState.map(([k, _]) => k)].reduce((prev, curr) => {
 	return prev.replace(RegExp(`[ \n]*${curr},?[ \n]*`), "")
     }, content.substring(defaultComponentParams.start as number, defaultComponentParams.end as number))}) => {`)
 
+    // indentation to use for the function body
     const baseIndentation = " ".repeat(defaultComponentBody.body[0].loc?.start.column as number)
 
+    // print selectors
     propsToState.forEach(([k, v]) => {
         console.log(`${baseIndentation}const ${k} = useSelector((state) => ${v})`);
     })
 
+    // print dispatchers
     console.log(`${baseIndentation}const dispatch = useDispatch()`)
-
     actions.forEach((name) => {
         console.log(`${baseIndentation}const dispatch${name[0].toUpperCase()}${name.substring(1)} = useCallback((...args) => dispatch(${name}(..args)), [dispatch])`);
     })
+
+    // print the rest of the body while replacing each actions with a dispatched version
 
     console.log()
 
@@ -121,6 +134,7 @@ const parseFile = (filePath: string) => {
 	return prev.replace(curr, `dispatch${curr[0].toUpperCase()}${curr.substring(1)}`)
     }, content.substring(defaultComponentBody.body[0].start as number, defaultComponentBody.end as number))}`)
 
+    // print everything after the default component declaration with connect remove
     console.log(`${content.substring(defaultComponentBody.end as number, rightBeforeConnect.start as number)}${defaultComponent}${content.substring(rightBeforeConnect.end as number)}`)
 }
 
